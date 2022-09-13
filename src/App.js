c9f73348111b52f6463db63d6da4df0d81197363
ashiikkhan <ashiksust06@gmail.com>
2022-09-13 11:20:17 +0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Clock from './components/Clock';
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <h1>This is App component</h1>
+      <Clock locale='bn-Bd' user='ashik'></Clock>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,40 +1,15 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-class Clock extends React.Component {
-  render() {
-    console.log(this.props);
-    const { locale, user, children } = this.props;
-    return (
-      <h1>
-        <span>
-          Hello {user} ! this is {children}{' '}
-          {new Date().toLocaleTimeString(locale)}
-        </span>
-      </h1>
-    );
-  }
-}
-
-ReactDOM.render(
-  <Clock locale='bn-BD' user='ashik'>
-    test
-  </Clock>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
